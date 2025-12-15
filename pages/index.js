40317b4a555dd49f64b8d79007c40de74b0c4a43
codinghub3473-lgtm <codinghub3473@gmail.com>
2025-12-15 14:43:12 +0500

    add home page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Website Live Hai ✅</h1>
+      <p>Admin panel open karne ke liye niche click karo</p>
+      <a href="/admin">Go to Admin</a>
+    </div>
+  )
+}
